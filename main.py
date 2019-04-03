4270cd690575b33ded7716cef1d8c9af91fff77d
s4lat <zakazchikm@gmail.com>
2019-04-03 22:26:58 +0500

    First realisation of desktop app(Game, not working)

--- main.py
+++ main.py
@@ -1,111 +1,98 @@
-from utils import *
+import sys, utils
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 import random as rand
-from colorama import init
-import os
 
-init()
+class MainWindow(QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.central_widget = QStackedWidget()
+		self.setCentralWidget(self.central_widget)
 
-line = "\033[36m-------------------------------------------\033[0m"
-game_frame = (
-"""
-%s
-\033[33;1m\t\t\t\tСчёт: %s
-Выберите подходящее под маску имя файла\033[0m
-%s
-\033[32;1mМаска: %s\033[0m
-\033[37;1mВарианты ответов:
-\t1. %s
-\t2. %s
-\t3. %s
-\t4. %s\033[0m
-%s
-""")
+		self.menu_widget = MenuWidget(self)
+		self.central_widget.addWidget(self.menu_widget)
+		self.menu_widget.exitBtn.clicked.connect(self.close)
+		self.menu_widget.playBtn.clicked.connect(self.Play)
 
-inter_frame = (
-"""
-%s
-\033[33;1m\t\t\tСчёт: %s\033[0m
-\033[37;1mВариант ответа %s - %s
-%s
-\033[37;1m\t1. %s
-\033[37;1m\t2. %s
-\033[37;1m\t3. %s
-\033[37;1m\t4. %s\033[0m
-%s
-""")
+	def Play(self):
+		game_widget = GameWidget(self)
+		self.central_widget.addWidget(game_widget)
+		self.central_widget.setCurrentWidget(game_widget)
+		game_widget.backBtn.clicked.connect(self.backToMenu)
 
-def cprint(s):
-	os.system('cls' if os.name == 'nt' else 'clear')
-	print(s)
-
-def intermFrame(inp, valid_answer, answers, mask, score):
-	if answers[inp] == valid_answer:
-		right = True
-		score += 1
-	else:
-		right = False
-		score -= 1
-
-	cprint(inter_frame % 
-		((line, 
-		"\033[32;1m%s + 1\033[0m" % (score - 1) if right else "\033[31;1m%s - 1\033[0m" % (score + 1),
-		inp + 1, "\033[32;1mВерный\033[0m" if right else "\033[31;1mНеверный\033[0m",
-		line,
-		) + tuple([answ + "  \033[32;1m<< Верный ответ\033[0m" if answ == valid_answer else answ + "  \033[31;1m<< Неверный ответ\033[0m" for answ in answers]) +
-		tuple([line])))
-
-	inp = input("\033[33;1mНажмите [ENTER], чтобы продолжить или q + [ENTER], чтобы выйти.\033[0m")
-
-	if inp == "q":
-		cprint("\033[33;1mИтоговый счет: %s\033[0m" % score)
-		return
-	else:
-		return gameFrame(score)
-
-
-
-
-def gameFrame(score):
-	mask = generate_mask()
-	answers = []
-
-	while len(answers) < 3:
-		answ = get_invalid_txt_by_mask(mask)
-		if answ not in answers:
-			answers.append(answ)
-
-	valid_answer = get_valid_txt_by_mask(mask)
-	answers.append(valid_answer)
-	rand.shuffle(answers)
-
-	cprint(game_frame % ((line, score, line, mask) + tuple(answers) + tuple([line])))
-	inp = ""
-	while True:
+	def backToMenu(self):
+		self.central_widget.setCurrentWidget(self.menu_widget)
 		
-		inp = input("Ваш ответ(q - выход): ")
 
-		try:
-			inp = int(inp)
-			if 1 <= inp <= 4:
-				inp -= 1
-		except ValueError:
-			if inp == "q":
-				break
-			else:
-				inp = ""
-				continue
+class MenuWidget(QWidget):
+	def __init__(self, parent=None):
+		super(MenuWidget, self).__init__(parent)
 
-		return intermFrame(inp, valid_answer, answers, mask, score)
+		self.playBtn = QPushButton("Начать")
+		self.settingsBtn = QPushButton("Настройки")
+		self.exitBtn = QPushButton("Выход")
 
-	cprint("Итоговый счет: %s" % score)
+		self.layout = QVBoxLayout()
+		self.layout.addWidget(self.playBtn)
+		self.layout.addWidget(self.settingsBtn)
+		self.layout.addWidget(self.exitBtn)
+		self.setLayout(self.layout)
 
-	
+class GameWidget(QWidget):
+	def __init__(self, parent=None):
+		super().__init__()
 
-def main():
-	score = 0
-	gameFrame(score)
+		self.backBtn = QPushButton("Вернуться")
+
+		self.replyBtn = QPushButton("Ответить")
+		self.replyBtn.clicked.connect(self.Reply)
+
+		self.layout = QVBoxLayout()
+
+		self.stage = Stage()
+
+		hbox = QHBoxLayout()
+		hbox.addWidget(QLabel("Выберите имя файла подходящее под маску"))
+		self.layout.addLayout(hbox)
+
+		hbox = QHBoxLayout()
+		hbox.addWidget(QLabel("Маска: " + self.stage.mask))
+		self.layout.addLayout(hbox)
+
+		hbox = QHBoxLayout()
+		hbox.addWidget(QLabel("Варианты ответов:"))
+		self.layout.addLayout(hbox)
+
+		answ_options = QButtonGroup()
+		for i, answ in enumerate(self.stage.answers):
+			hbox = QHBoxLayout()
+			opt = QCheckBox(answ)
+			answ_options.addButton(opt)
+			hbox.addWidget(opt)
+			self.layout.addLayout(hbox)
 
 
-		
+		hbox = QHBoxLayout()
+		hbox.addWidget(self.backBtn)
+		hbox.addWidget(self.replyBtn)
+		self.layout.addLayout(hbox)
+
+		self.setLayout(self.layout)
+
+	def Reply(self):
+		pass
+
+class Stage:
+	def __init__(self):
+		self.mask = utils.generate_mask()
+		self.answers = [utils.get_invalid_txt_by_mask(self.mask) for i in range(3)]
+		self.valid_answer = utils.get_valid_txt_by_mask(self.mask)
+		self.answers.append(self.valid_answer)
+		rand.shuffle(self.answers)
+
+
+
+
 
 
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from main import main
+from main import MainWindow
+from PyQt5.QtWidgets import QApplication
+import sys
 
-if __name__ == "__main__":
-	main()
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	win = MainWindow()
+	win.show()
+	sys.exit(app.exec_())
