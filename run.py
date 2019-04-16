20aed3836dac68feee700e772d3f06ade9a0c41a
s4lat <zakazchikm@gmail.com>
2019-04-16 07:42:09 +0300

    kek

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from PyQt5.QtWidgets import QApplication
 import sys
 
 if __name__ == '__main__':
+	pyqt = os.path.dirname(PyQt5.__file__)
+	QApplication.addLibraryPath(os.path.join(pyqt, "plugins"))
+	
 	app = QApplication(["MaskTrainer"])
 	win = MainWindow()
 	win.show()
