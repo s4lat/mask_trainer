da446b501241529c77231933d4cd40fc4ff410e0
s4lat <zakazchikm@gmail.com>
2019-04-16 17:52:26 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,7 @@ from main import MainWindow
 from PyQt5.QtWidgets import QApplication
 import sys, os, PyQt5
 
-if __name__ == '__main__':
-	pyqt = os.path.dirname(PyQt5.__file__)
-	QApplication.addLibraryPath(os.path.join(pyqt, "plugins"))
-	
+if __name__ == '__main__':	
 	app = QApplication(["MaskTrainer"])
 	win = MainWindow()
 	win.show()
